c1ad4cd8c793f7f7fb1eb285b656f18cea48b236
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-06-19 02:23:21 +0200

    test commit

--- src/components/3dcomponents/genObject.tsx
+++ src/components/3dcomponents/genObject.tsx
@@ -3,6 +3,13 @@ import { BoxGeometry, MeshNormalMaterial, Mesh } from "three";
 export function genCube(): Mesh {
   const geometry = new BoxGeometry(1, 1, 1);
   const material = new MeshNormalMaterial({ flatShading: true });
+
+
+
+
+
+
+
   const cube = new Mesh(geometry, material);
   return cube;
 }
